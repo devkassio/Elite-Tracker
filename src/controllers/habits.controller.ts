c154dfd1b12243bee4ad6ade302b701c8fd352fe
devkassio <kassioxis@icloud.com>
2025-11-25 16:49:41 -0300

    Adiciona header de copyright no controller de hábitos

--- src/controllers/habits.controller.ts
+++ src/controllers/habits.controller.ts
@@ -1,3 +1,13 @@
+/**
+ * Elite Tracker API - Habits Controller
+ * 
+ * Copyright (c) 2025 Kássio Barros (@devkassio)
+ * Licensed under MIT License
+ * 
+ * @author Kássio Barros
+ * @repository https://github.com/devkassio/Elite-Tracker
+ */
+
 import type { Request, Response } from 'express';
 
 export class HabitsController {
