491ad1822fa182af60ecdecf14479fdbb9368b95
devkassio <kassioxis@icloud.com>
2025-11-26 12:23:01 -0300

    Adiciona validação de dados com Zod no controller de hábitos

--- src/controllers/habits.controller.ts
+++ src/controllers/habits.controller.ts
@@ -9,18 +9,32 @@
  */
 
 import type { Request, Response } from 'express';
+import { z } from 'zod';
 import { habitModel } from '../schemas/habit.model.js';
 
 export class HabitsController {
   store = async (req: Request, res: Response): Promise<Response> => {
+    const Schema = z.object({
+      name: z.string().min(2).max(70),
+    });
+
     const { name } = req.body;
-    const findHabit = await habitModel.findOne({ name });
+    const habit = Schema.safeParse({ name });
+
+    if (!habit.success) {
+      return res.status(400).json({ message: 'Invalid habit name.' });
+    }
+
+    const findHabit = await habitModel.findOne({ name: habit.data.name });
 
     if (findHabit) {
       return res.status(400).json({ message: 'Habit already exists.' });
     }
 
-    const newHabit = await habitModel.create({ name, isCompleted: [] });
+    const newHabit = await habitModel.create({
+      name: habit.data.name,
+      isCompleted: [],
+    });
 
     return res.status(201).json(newHabit);
   };
